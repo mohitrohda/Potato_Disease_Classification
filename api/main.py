cadabe67b55ef8b08f761df0d1634d8e8ebb4ca9
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-04-06 15:55:11 +0530

    improvr backend for deployment

--- api/main.py
+++ api/main.py
@@ -4,7 +4,6 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 import tensorflow as tf
-from class_names import CLASS_NAMES
 
 app = FastAPI()
 
@@ -18,6 +17,30 @@ app.add_middleware(
 
 
 MODEL = tf.keras.models.load_model("../saved_models/1.keras")
+
+CLASS_NAMES = ['Apple___Apple_scab',
+ 'Apple___Black_rot',
+ 'Apple___Cedar_apple_rust',
+ 'Apple___healthy',
+ 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+ 'Corn_(maize)___Common_rust_',
+ 'Corn_(maize)___Northern_Leaf_Blight',
+ 'Corn_(maize)___healthy',
+ 'Pepper__bell___Bacterial_spot',
+ 'Pepper__bell___healthy',
+ 'Potato___Early_blight',
+ 'Potato___Late_blight',
+ 'Potato___healthy',
+ 'Tomato_Bacterial_spot',
+ 'Tomato_Early_blight',
+ 'Tomato_Late_blight',
+ 'Tomato_Leaf_Mold',
+ 'Tomato_Septoria_leaf_spot',
+ 'Tomato_Spider_mites_Two_spotted_spider_mite',
+ 'Tomato__Target_Spot',
+ 'Tomato__Tomato_YellowLeaf__Curl_Virus',
+ 'Tomato__Tomato_mosaic_virus',
+ 'Tomato_healthy']
 
 def read_file_as_img(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
